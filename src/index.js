f7d7fc1b5a17eebf8e1ffc9e89f271939cd73ced
cindy <hsuan@192-168-1-114.tpgi.com.au>
2019-07-24 17:47:23 +1000

    app listen
    Committer: cindy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+//once you have assigned a value to a variable by const, you can't reassign a new value to it.
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>console.info('Server start to listen on ${PORT}'));
+
+
+//${PORT}
+//app.listen(port number,call back)
+app.listen(PORT,()=>console.info('Server start to listen on '+ PORT ));
