76ddeb96646942bb3ab0bebcf38af99bfa077cfb
cindy <hsuan@192-168-1-114.tpgi.com.au>
2019-07-24 17:13:02 +1000

    app listen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello Node API");
+let express = require('express');
+let app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>console.info('Server start to listen on ${PORT}'));
