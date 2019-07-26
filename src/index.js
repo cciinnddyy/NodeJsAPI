9a9856b09f620cea633ae4967b62cb01604aa6ee
cindy <hsuan@192-168-1-114.tpgi.com.au>
2019-07-26 11:39:58 +1000

    add files to .igonre

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ let app = express();
 
 //once you have assigned a value to a variable by const, you can't reassign a new value to it.
 
+
+//run the command PORT=4444 process.env.PORT = 4444
 const PORT = process.env.PORT || 3000;
 
 
 
+console.log(process.env.PORT);
+
 //${PORT}
-//app.listen(port number,call back)
+//app.listen(port number,call back())
 app.listen(PORT,()=>console.info('Server start to listen on '+ PORT ));
+
+ 
